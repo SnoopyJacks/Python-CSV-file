d0b8b004923819ce7944c80d0211e054cdbbf103
SnoopyJacks <t.nichole.2807@gmail.com>
2024-12-30 10:58:15 -0700

    organizing the code better

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import csv
 import matplotlib.pyplot as plt
 
-filename = 'data.csv'
+def generate_population_dictionary_from_csv(filename):
+    """ Generate a dictionary with the population per continent from a csv file """
+    output = {}
 
-population_per_continent = {}
 
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for line in reader:
+            continent = line['continent']
+            year = line['year']
+            population = line['population']
 
-with open(filename, 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for line in reader:
-        continent = line['continent']
-        year = line['year']
-        population = line['population']
+            if continent not in output:
+                output[continent] = {'population': [], 'years': []}
 
-        if continent not in population_per_continent:
-            population_per_continent[continent] = {'population': [], 'years': []}
+            output[continent]['population'].append(population) 
+            output[continent]['years'].append(year)
+    return output
 
-        population_per_continent[continent]['population'].append(population) 
-        population_per_continent[continent]['years'].append(year)
-
-for continent in population_per_continent:
-    years = population_per_continent[continent]['years']
-    population = population_per_continent[continent]['population']
-    plt.plot(years,population, label=continent, marker="o", alpha=0.5)
+def generate_population_plot_from_dictionary(population_dictionary):
+    
+    for continent in population_dictionary:
+        years = population_dictionary[continent]['years']
+        population = population_dictionary[continent]['population']
+        plt.plot(years,population, label=continent, marker="o", alpha=0.5)
 
 plt.title("Internet Population per Continent")
 plt.xlabel("Year")
@@ -32,3 +35,8 @@ plt.legend()
 plt.tight_layout()
 
 plt.show()
+
+filename = 'data.csv'
+
+population_per_continent = generate_population_dictionary_from_csv(filename)
+generate_population_plot_from_dictionary(population_per_continent)
