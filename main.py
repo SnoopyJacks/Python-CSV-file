da0833e8c88d5f5ccf85c07ad01282a5b66a6578
SnoopyJacks <t.nichole.2807@gmail.com>
2024-12-30 10:47:18 -0700

    organizing data information so that it can be plotted in a chart

--- main.py
+++ main.py
@@ -19,13 +19,16 @@ with open(filename, 'r') as csvfile:
         population_per_continent[continent]['population'].append(population) 
         population_per_continent[continent]['years'].append(year)
 
-
-plt.plot([2000, 2001, 2002], [1990, 1991, 1992], label='Europe')
-plt.plot([2000, 2001, 2002], [1990, 1991, 1992], label='Asia')
+for continent in population_per_continent:
+    years = population_per_continent[continent]['years']
+    population = population_per_continent[continent]['population']
+    plt.plot(years,population, label=continent, marker="o", alpha=0.5)
 
 plt.title("Internet Population per Continent")
 plt.xlabel("Year")
 plt.ylabel("Internet Users")
 plt.grid(True)
 plt.legend()
+plt.tight_layout()
+
 plt.show()
