6716b141ce5fe0fb1a3fe229ae3288c50f9889bf
SnoopyJacks <t.nichole.2807@gmail.com>
2024-12-30 11:02:33 -0700

    trying to fix the plot
    Plot is showing no  information 👎

--- main.py
+++ main.py
@@ -5,38 +5,34 @@ def generate_population_dictionary_from_csv(filename):
     """ Generate a dictionary with the population per continent from a csv file """
     output = {}
 
-
     with open(filename, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         for line in reader:
             continent = line['continent']
-            year = line['year']
-            population = line['population']
+            year = int(line['year'])
+            population = int(line['population'])
 
             if continent not in output:
-                output[continent] = {'population': [], 'years': []}
-
-            output[continent]['population'].append(population) 
+                output[continent] = {'years': [], 'population': []}
             output[continent]['years'].append(year)
+            output[continent]['population'].append(population)
+
     return output
 
 def generate_population_plot_from_dictionary(population_dictionary):
-    
     for continent in population_dictionary:
         years = population_dictionary[continent]['years']
         population = population_dictionary[continent]['population']
-        plt.plot(years,population, label=continent, marker="o", alpha=0.5)
+        plt.plot(years, population, label=continent, marker="o", alpha=0.5)
 
-plt.title("Internet Population per Continent")
-plt.xlabel("Year")
-plt.ylabel("Internet Users")
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-
-plt.show()
+    plt.title("Internet Population per Continent")
+    plt.xlabel("Year")
+    plt.ylabel("Internet Users")
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
 
 filename = 'data.csv'
-
 population_per_continent = generate_population_dictionary_from_csv(filename)
 generate_population_plot_from_dictionary(population_per_continent)
