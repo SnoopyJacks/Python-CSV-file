29228804ca1e3321db325ef8cdee4aa18b410cc3
SnoopyJacks <t.nichole.2807@gmail.com>
2024-12-30 10:31:35 -0700

    Added data manipulation calculations

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 filename = 'data.csv'
 
+population_per_continent = {}
+
+
 with open(filename, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for line in reader:
@@ -10,10 +13,11 @@ with open(filename, 'r') as csvfile:
         year = line['year']
         population = line['population']
 
-        print(continent)
-        print(year)
-        print(population)
+        if continent not in population_per_continent:
+            population_per_continent[continent] = {'population': [], 'years': []}
 
+        population_per_continent[continent]['population'].append(population) 
+        population_per_continent[continent]['years'].append(year)
 
 
 plt.plot([2000, 2001, 2002], [1990, 1991, 1992], label='Europe')
@@ -23,4 +27,5 @@ plt.title("Internet Population per Continent")
 plt.xlabel("Year")
 plt.ylabel("Internet Users")
 plt.grid(True)
+plt.legend()
 plt.show()
